ef4c8f75063a9a4922b747adc668f2bf67928a2b
0K0 <qdouvillez@gmail.com>
2021-03-06 20:56:51 +0100

    ca swell fort

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Titillium Web','Roboto', 'Helvetica', 'Arial', 'sans-serif']
-      }
-    },
-  },
-  variants: {},
-  plugins: []
-}
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './compoments/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Titillium Web', 'Roboto', 'Helvetica', 'Arial', 'sans-serif']
+			}
+		}
+	},
+	variants: {},
+	plugins: []
+};
